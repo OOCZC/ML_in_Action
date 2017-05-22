2083f152715f4937ba447f54bb679e31f6a21010
OOC <whatisooc@gmail.com>
2017-05-22 13:59:35 +0800

    Update kNN.py

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -22,11 +22,6 @@ def classify0(inX, dataSet, labels, k):
  key=operator.itemgetter(1), reverse=True)
 	return sortedClassCount[0][0]
 
-def createDataSet():
-	group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-	labels = ['A','A','B','B']
-	return group, labels
-
 def file2matrix():
 	fr = open('datingTestSet.txt')
 	arrayLines = fr.readlines()
@@ -36,6 +31,7 @@ def file2matrix():
 	index = 0
 	for line in arrayLines:
 		line = line.strip()
+		#去除开头结尾空白字符
 		listFromLine = line.split('\t')
 		returnMat[index,:] = listFromLine[0:3]
 		#returnMat[index,:]把三个数据放在index行，从0列开始放
@@ -47,24 +43,46 @@ def autoNorm(dataSet):
 	#归一化数值
 	#dataSet为n*3矩阵
 	minVals = dataSet.min(0)
-	print minVals
+	#print minVals
 	#从列中选取最小值，而不是当前行最小值，行最小min(1)
 	maxVals = dataSet.max(0)
-	print maxVals
+	#print maxVals
 	ranges = maxVals - minVals
-	print ranges
+	#print ranges
 	normDataSet = np.zeros(np.shape(dataSet))
 	#shape(dataSet)返回矩阵的形状,eg: (3,4)
 	m = dataSet.shape[0] #dataSet第一维的长度
-	print m
+	#print m
 	normDataSet = dataSet - np.tile(minVals, (m,1))
 	normDataSet = normDataSet/np.tile(ranges, (m,1))
-	print normDataSet 
+	#print normDataSet 
 	return normDataSet, ranges, minVals
 
-datingDataMat,datingLabels = file2matrix()
-autoNorm(datingDataMat)
+def datingClassTest():
+	hoRatio = 0.05
+	datingDataMat,datingLabels = file2matrix()
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	#m是有多少行
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:],\
+	normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+		print 'the classifier came back with : %d, the\
+	real answer is: %d' % (classifierResult, datingLabels[i])
+		if (classifierResult != datingLabels[i]):
+			errorCount += 1.0
+	print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
+	
+datingClassTest()
 '''
+datingDataMat,datingLabels = file2matrix()
+normMat, ranges, minVals = autoNorm(datingDataMat)
+print normMat
+print ranges
+print minVals
+
 group, labels = createDataSet()
 anw = classify0([2,3], group, labels, 3)
 print anw

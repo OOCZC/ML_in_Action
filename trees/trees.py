073a479ca285bd56fbd6a617c777edde84e9b839
OOC <whatisooc@gmail.com>
2017-06-03 20:44:23 +0800

    update trees.py

--- trees/trees.py
+++ trees/trees.py
@@ -6,7 +6,11 @@ import operator
 import matplotlib
 import matplotlib.pyplot as plt
 from math import log
-
+'''
+计算香农熵，只计算标签。
+dataSet是二维List
+即[[1,0,1],[0,1,1],[0,0,1]]
+'''
 def calcShannonEnt(dataSet):
 	numEntries = len(dataSet)
 	labelCounts = {}
@@ -21,15 +25,22 @@ def calcShannonEnt(dataSet):
 		shannonEnt -= prob * log(prob, 2)
 	return shannonEnt
 
+'''
+dataSet的最后一个是后面需要判断的属性
+'''
 def createDataSet():
 	dataSet = [[1, 1, 'yes'],
 		[1, 1, 'yes'],
 		[1, 0, 'no'],
 		[0, 1, 'no'],
 		[0, 1, 'no']]
+	#通过前两个属性，判断最后一个。
 	labels = ['no surfacing', 'flippers']	
+	#前两个属性是什么，后面不太需要用到
 	return dataSet, labels
-
+'''
+把axis轴值为value的数据分割出来，axis轴的数据不保留
+'''
 def splitDataSet(dataSet, axis, value):
 	retDataSet = []
 	for featVec in dataSet:
@@ -40,7 +51,9 @@ def splitDataSet(dataSet, axis, value):
 			retDataSet.append(reducedFeatVec)
 	return retDataSet
 
-
+'''
+选择最好的划分数据集的特征
+'''
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1      
     #the last column is used for the labels
@@ -69,7 +82,11 @@ def chooseBestFeatureToSplit(dataSet):
     return bestFeature                
     #returns an integer
 
-####
+'''
+classList 是一维List
+返回最多出现的标签。
+即多数表决。
+'''
 
 def majorityCnt(classList):
     classCount={}
@@ -79,23 +96,46 @@ def majorityCnt(classList):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+'''
+创建决策树。
+两个参数与createDataSet返回的参数相同
+'''
+
 def createTree(dataSet,labels):
+    #函数参数是列表类型时，按引用方式传递，即改变原值。
     classList = [example[-1] for example in dataSet]
     if classList.count(classList[0]) == len(classList): 
         return classList[0]#stop splitting when all of the classes are equal
-    if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
+    if len(dataSet[0]) == 1: 
+        #stop splitting when there are no more features in dataSet
+        #这里只剩最后的判断属性了。没有特征了
         return majorityCnt(classList)
     bestFeat = chooseBestFeatureToSplit(dataSet)
     bestFeatLabel = labels[bestFeat]
+    #bestFeatLabel是标签的名字 
     myTree = {bestFeatLabel:{}}
+    #字典存储无序键值对
+    #myTree[bestFeatLabel]={}
+    #等价与上面的语句，都是向字典中添加键值对。这里的值为空字典
     del(labels[bestFeat])
+    #删除list中的这个元素
     featValues = [example[bestFeat] for example in dataSet]
     uniqueVals = set(featValues)
     for value in uniqueVals:
-        subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+        subLabels = labels[:] 
+        #copy all of labels, so trees don't mess up(弄乱) existing labels
+        #如果直接给下面的函数参数labels，则每次经过myTree的labels都将不同。
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
+        #这里myTree[bestFeatLabel]对应的仍为一个字典
     return myTree                            
-    
+
+
+dataSet, labels = createDataSet()
+trees = createTree(dataSet, labels)
+print trees
+
+
+'''
 def classify(inputTree,featLabels,testVec):
     firstStr = inputTree.keys()[0]
     secondDict = inputTree[firstStr]
@@ -123,3 +163,4 @@ def grabTree(filename):
 
 myDat, label = createDataSet()
 print calcShannonEnt(myDat)
+'''
